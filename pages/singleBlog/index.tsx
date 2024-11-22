160a65d1384eca6f964c22fa6b47761509003601
Ashish Raj <rashish0809.ar@gmail.com>
2024-11-22 13:04:18 +0530

    added card readmore fic

--- pages/singleBlog/index.tsx
+++ pages/singleBlog/index.tsx
@@ -9,7 +9,7 @@ import BlogCard from '@/components/BlogCard'
 const index = () => {
   return (
     <AppWrapper isSearchEnabled={true} padding={true} navbarEnable={true}>
-        <div className='p-12 w-4/5 shadow-2xl transition-shadow duration-300 flex flex-col gap-8 mt-24'>
+        <div className='w-full p-8 shadow-2xl transition-shadow duration-300 flex flex-col justify-center items-center gap-8 lg:mt-24 md:mt-18'>
             <BlogCard isReadMoreEnabled={false}/>
             
         </div>
